8636b5f6bc4b6852b642c65cd8f4d454e7eaeaa6
Robson Pires <robsonvpires@gmail.com>
2021-01-01 14:03:02 -0300

    0101 update

--- final_project/views.py
+++ final_project/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+matches = [
+    {
+        'local': 'Grêmio',
+        'visitor': 'Inter',
+        'place': 'Arena do Grêmio',
+        'date': '16/10/20'
+    },
+    {
+        'local': 'Juventude',
+        'visitor': 'Caxias',
+        'place': 'Alfredo Jaconi',
+        'date': '17/10/20'
+    },
+]
+def home(request):
+
+    context = {
+        'matches': matches
+    }
+    return render(request, 'final_project/home.html', context)
+
+def about(request):
+    return render(request, 'final_project/about.html')
