6072b22f1eeffa43e80d4b4711e1dfb71e9d264f
Robson Pires <robsonvpires@gmail.com>
2021-01-03 13:22:34 -0300

    Create of login,logout, register functionality

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+def register(request):
+
+    #check if request was created by method POST if positive it fills the form with the POST data
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
